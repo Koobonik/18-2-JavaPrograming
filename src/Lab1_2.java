9a841359a36fadddce0594f79701bc5e86f018ce
Koobonik <kbi960130@naver.com>
2018-09-07 14:07:27 +0900

    과제 완료

--- src/Lab1_2.java
+++ src/Lab1_2.java
@@ -8,28 +8,44 @@ public class Lab1_2 {
         System.out.println("=================");
 
 
-        // 돈 변수
+        // 해당 돈에 해당하는 장수를 저장해둘 변수
         int money_50000, money_10000, money_5000, money_1000, money_500, money_100, money_Other;
 
 
         //금액 입력
         System.out.println("돈을 입력해주세요: ");
-        int money = sc.nextInt(); // 이름 입력
+        int money = sc.nextInt(); // 금액 입력
 
         System.out.println("------결과------");
 
-        money_50000 = money/50000;
-        System.out.println("50000원 : " + money_50000 + "장"); // 5만원권 나눠주기
-
-        // 1만원권 장수 구하기
-        money -= money_50000;
-        System.out.println("10000원 : " + money);
+        money_50000 = money/50000; // 5만원권 장수 구하기
+        System.out.println("50000원 : " + money_50000 + "장");
 
 
-        System.out.println("5000원 : " +money);
-        System.out.println("1000원 : " );
-        System.out.println("500원 : " );
-        System.out.println("100원 : " );
+        money -= 50000 * money_50000; // 5만원의 장수만큼 빼준값을 다시 money 변수에 대입
+        money_10000 = money/10000; // 1만원권 장수 구하기
+        System.out.println("10000원 : " + money_10000 + "장");
+
+        money -= 10000 * money_10000; // 1만원권의 장수만큼 빼준값을 다시 money 변수에 대입
+        money_5000 = money/5000; // 5천원권 장수 구하기
+        System.out.println("5000원 : " + money_5000 + "장");
+
+        money -= 5000 * money_5000; // 5천원권의 장수만큼 빼준값을 다시 money 변수에 대입
+        money_1000 = money/1000; // 1천원권 장수 구하기
+        System.out.println("1000원 : " + money_1000 + "장" );
+
+        money -= 1000 * money_1000; // 1천원권의 장수만큼 빼준값을 다시 money 변수에 대입
+        money_500 = money/500; // 500원 개수 구하기
+        System.out.println("500원 : " + money_500 + "장");
+
+        money -= 500 * money_500; // 5백원권의 장수만큼 빼준값을 다시 money 변수에 대입
+        money_100 = money/100; // 100원 개수 구하기
+        System.out.println("100원 : " + money_100 + "장");
+
+        money -= 100*money_100; // 백원 미만으로 남은 금액 대입
+
+        if(money != 0) // 0원 아니면 남은 금액 출력하고 조건 만족안하면 출력 x
+        System.out.println("기타 : " + money + "원");
 
     }
 }
