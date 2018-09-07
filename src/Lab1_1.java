1604df0a18bfee91bec4112379c74ba9317419ed
Koobonik <kbi960130@naver.com>
2018-09-07 13:09:57 +0900

    중간 커밋

---
+++ src/Lab1_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Lab1_1 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=================");
+        System.out.println("201504003 구본익");
+        System.out.println("=================");
+
+        System.out.println("이름을 입력해주세요: ");
+        String name = sc.next();
+
+        System.out.println(name + "님 학과를 입력해주세요: ");
+        String major = sc.next();
+
+        System.out.println("------점수를 입력해주세요------");
+        System.out.println("국어: ");
+        3
+    }
+}
