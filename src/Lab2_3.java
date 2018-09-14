ad921b299ff79b456d8ae6cc162258ee88cdce88
Koobonik <kbi960130@naver.com>
2018-09-14 17:00:27 +0900

    과제 완료

--- src/Lab2_3.java
+++ src/Lab2_3.java
@@ -16,22 +16,29 @@ public class Lab2_3 {
         int num;
 
         do{
-            System.out.println("1 ~ 9 숫자를 입력해주세요");
+            System.out.println("1 ~ 9 숫자를 입력해주세요 (끝내려면 0)");
             num = sc.nextInt();
 
-            for (int i = num; i>0 ;i--) // 초기값을 num으로 줘서 띄어쓰기 먼저 주기 // 띄어쓰기 파트
+            for (int i = 0; i<num; i++) // 초기값을 num으로 줘서 띄어쓰기 먼저 주기 // 띄어쓰기 파트
             {
-                for (char b = 'a'; b<='z'; b++){
-                    System.out.println(b);
+                if(num>9 || num<1)
+                {
+                    // 9보다 크거나 1보다 작으면
+                    System.out.println("잘못입력하셨습니다.");
+                    break;
                 }
+                for(int j = num; j> i+1; j--) // 띄어쓰기를 관리해줄 for문
+                {
+                    System.out.print(" ");
+                }
+                for (char b = 65; b<=65+(i*2); b++) // 문자가 2개씩 추가될 것이기에 i*2를 해준다.
+                {
+                    System.out.print(b);
+                }
+                System.out.println();
+
             }
-
         }while(num != 0);
-
-
-//        for (char b = 'a'; b<='z'; b++){
-//            System.out.println(b);
-//        }
 
     }
 }
