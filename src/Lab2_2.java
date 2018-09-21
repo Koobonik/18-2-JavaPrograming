ff61b518037e598d1771b183b0b6aa77e7338bd1
Koobonik <kbi960130@naver.com>
2018-09-21 09:17:06 +0900

    소스수정

--- src/Lab2_2.java
+++ src/Lab2_2.java
@@ -17,37 +17,42 @@ public class Lab2_2 {
         System.out.println("생년월일을 입력해주세요: (ex: 960130)");
         String birthday = sc.nextLine();
 
-        // 생일의 길이가 6이 아니라면
-        if(birthday.length() != 6)
-            System.out.println("입력형식이 잘못되었습니다.");
-
-        birthday = birthday.substring(2,4);
-        int month = Integer.parseInt(birthday);
-
-        switch (month)
+        if(birthday.length() == 6) // 생일의 크기가 6일때에만 실행
         {
-            case 12 : case 1 : case 2:
+            birthday = birthday.substring(2,4);
+            int month = Integer.parseInt(birthday);
+
+            switch (month)
+            {
+                case 12 : case 1 : case 2:
             {
                 System.out.println("겨울에 태어나셨군요");
                 break;
             }
-            case 3 : case 4 : case 5:
+                case 3 : case 4 : case 5:
             {
                 System.out.println("봄에 태어나셨군요");
                 break;
             }
-            case 6 : case 7 : case 8:
+                case 6 : case 7 : case 8:
             {
                 System.out.println("여름에 태어나셨군요");
                 break;
             }
-            case 9 : case 10 : case 11:
+                case 9 : case 10 : case 11:
             {
                 System.out.println("가을에 태어나셨군요");
                 break;
             }
-            default:
-                System.out.println("월 형식이 잘못되었습니다.");
+                default:
+                    System.out.println("월 형식이 잘못되었습니다.");
+            }
         }
+        // 생일의 길이가 6이 아니라면
+        else {
+            System.out.println("입력형식이 잘못되었습니다.");
+        }
+
+
     }
 }
