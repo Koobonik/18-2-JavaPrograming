c061a294dfa71ff657641f36fc34cdff1224f08f
Koobonik <kbi960130@naver.com>
2018-10-05 09:40:20 +0900

    samp 추가

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,9 @@ public class Main {
         //ArrayLength();
         //foreachEx();
         //SalesRevenue();
-        ReturnArray();
+        //ReturnArray();
+        Samp a = new Samp(2);
+        a.trans();
         
     }
 
@@ -363,6 +365,24 @@ public class Main {
             this.age = n;
             n++;
         }
+    }
+    public static class Samp{
+        int id;
+        public Samp(int x) {this.id = x;}
+        public void set (int x) {this.id = x;}
+        public int get() {return  this.id;}
+
+        public void trans(){
+            Samp ob1 = new Samp(3);
+            Samp ob2 = new Samp(4);
+            Samp s;
+
+            s = ob2;
+            ob1 = ob2;
+            System.out.println("ob1.id=" + ob1.id);
+            System.out.println("ob1.id=" + ob2.id);
+        }
+
     }
 
 }
