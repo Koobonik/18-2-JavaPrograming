2e6369d4c0be29adcc7fdf57a0252506134bbcc0
Koobonik <kbi960130@naver.com>
2018-10-26 13:49:43 +0900

    중간 커밋

--- src/Lab6/Hero.java
+++ src/Lab6/Hero.java
@@ -3,16 +3,11 @@ package Lab6;
 import java.util.Scanner;
 
 abstract public class Hero {
-    private String name;
-    private String position;
-
-
-    private int shot_num;
-
-
-
-    private int gauge;
-    private int life;
+    private String name; // 영웅 이름
+    private String position; // 영웅 포지션
+    private int shot_num; // 영웅 총알 수
+    private int gauge; // 영웅 궁극기 게이지
+    private int life; // 영웅 체력
 
     public Hero(String name, int life, int shot_num, String position){
         this.name = name;
@@ -24,20 +19,19 @@ abstract public class Hero {
     public void setShot_num(int shot_num) {
         this.shot_num = shot_num;
     }
-
     public int getShot_num(){
         return shot_num;
     }
+
     public int getGauge() {
         return gauge;
     }
-
     public void setGauge(int gauge) {
         this.gauge = gauge;
     }
+
     public void info(){
         System.out.println(name);
     }
     abstract public void menu(Scanner kbd);
-
 }
---
+++ src/Lab6/Lab6.java
@@ -0,0 +1,27 @@
+package Lab6;
+
+import java.util.Scanner;
+
+/*
+ *
+ * 201504003 구본익
+ * 2018-10-31 24:00 까지 과제
+ *
+ * */
+
+public class Lab6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=================");
+        System.out.println("201504003 구본익");
+        System.out.println("=================");
+
+
+        System.out.println("영웅을 선택해주세요");
+        System.out.println("1. 솔저   2. 트레이서      0. 끝내기");
+        Hero hero;
+        //hero.menu(sc);
+
+
+    }
+}
