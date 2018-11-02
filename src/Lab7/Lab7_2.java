147413af8cb26ea57463e4a6962c7464fac252a1
Koobonik <kbi960130@naver.com>
2018-11-02 17:13:05 +0900

    과제 완료

--- src/Lab7/Lab7_2.java
+++ src/Lab7/Lab7_2.java
@@ -15,20 +15,16 @@ public class Lab7_2 {
         System.out.println("201504003 구본익 컴퓨터소프트웨어학과");
         System.out.println("=================");
         System.out.println("(안!녕@하^세~요)" + " -> " + delChar("(안!녕@하^세~요)", "~!@#$%^&*()"));
-        System.out.println("(2 0 1 8\t년\t11 월 2일)" + " -> " + delChar("(2 0 1 8\t년\t11 월 2일)", " \t()"));
+        System.out.println("(2 0 1 8\t년\t11 월 2일)" + " -> " + delChar("(2 0 1 8\t년\t11 월 2일)", " \t"));
     }
     static String delChar(String str1, String str2){
+        // 스트링 토크나이저 이용해서 특수문자 제거
         StringTokenizer str = new StringTokenizer(str1, str2);
         String sum = "";
-        for(int i = 0 ; i < str1.length(); i++){
-
-
-        }
         while (str.hasMoreTokens()){
-            System.out.print(str.nextToken());
+            // 토큰을 바로 주면 안됨. 문자로 변환해주고 보내줘야함
+            sum += str.nextToken().toString();
         }
-
-
         return sum;
     }
 }
