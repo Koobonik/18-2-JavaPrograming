2417b9d73266b352bbb1d1de92400e62b25a1a4a
Koobonik <kbi960130@naver.com>
2018-11-02 10:50:47 +0900

    String EX

---
+++ src/JavaExam/StringEx/StringEX.java
@@ -0,0 +1,26 @@
+package JavaExam.StringEx;
+
+public class StringEX {
+    public static void main(String [] args) {
+        String a = new String(" abcd");
+        String b = new String(",efg");
+
+        // 문자열 연결
+        a = a.concat(b);
+        System.out.println(a);
+
+        // 공백 제거
+        a = a.trim();
+        System.out.println(a);
+
+        // 문자열 대치
+        a = a.replace("ab", "12");
+        System.out.println(a);
+
+        // 문자열 분리
+        String s[] = a.split(",");
+        for (int i = 0; i< s.length; i++){
+            System.out.println("분리된 " + i + "번 문자열: " + s[i]);
+        }
+    }
+}
