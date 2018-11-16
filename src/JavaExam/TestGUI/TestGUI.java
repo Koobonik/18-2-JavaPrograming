6b2c5e58b10e603f3d27dbcde85f93bfb1fa42ff
Koobonik <kbi960130@naver.com>
2018-11-16 11:01:37 +0900

    GUI 연습

---
+++ src/JavaExam/TestGUI/TestGUI.java
@@ -0,0 +1,16 @@
+package JavaExam.TestGUI;
+
+import java.awt.*;
+import javax.swing.*;
+public class TestGUI extends JFrame{
+    TestGUI(){
+        setTitle("GUI 연습");
+        add(new JButton("OK"));
+        add(new JButton("x"));
+        setSize(300, 100);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new TestGUI();
+    }
+}
