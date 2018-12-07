36aa6a025424be80ce5fc1189fc6744b8fd08a42
Koobonik <kbi960130@naver.com>
2018-12-07 10:14:55 +0900

    버튼 예제

---
+++ src/JavaExam/day12_07/Test2.java
@@ -0,0 +1,30 @@
+package JavaExam.day12_07;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Test2 extends JFrame {
+    JLabel label = new JLabel();
+    JTextField text;
+    Test2(){
+        setTitle("버튼예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        text = new JTextField(10);
+        text.addActionListener(new text());
+        add(label, BorderLayout.CENTER);
+        add(text, BorderLayout.NORTH);
+        setSize(200,200);
+        setVisible(true);
+    }
+    class text implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e){
+            label.setText(text.getText());
+        }
+    }
+    public static void main(String []args){
+        new Test2();
+    }
+}
