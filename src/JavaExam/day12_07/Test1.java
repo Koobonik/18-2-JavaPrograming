5548fafd4fdb0db3417f812e2ad51ecacce5264a
Koobonik <kbi960130@naver.com>
2018-12-07 09:37:49 +0900

    주석 추가

--- src/JavaExam/day12_07/Test1.java
+++ src/JavaExam/day12_07/Test1.java
@@ -11,8 +11,8 @@ public class Test1 extends JFrame {
         setLayout(new FlowLayout());
 
         ButtonGroup g = new ButtonGroup();
-        JRadioButton apple = new JRadioButton("apple");
-        JRadioButton cherry = new JRadioButton("cherry");
+        JRadioButton apple = new JRadioButton("apple", true); // true 일경우 선택이 되어있음
+        JRadioButton cherry = new JRadioButton("cherry"); // 다른곳에도 true가 설정될 경우 맨처음에 true를 받은 애만 선택됨
         JRadioButton apple2 = new JRadioButton("apple2");
         g.add(apple);
         g.add(cherry);
